d45a96ece7ac2a7102d2f3f1de17f3b6eb57feac
Andrey Mozhaev <Mozhaev.andrey.v@yandex.com>
2025-02-05 23:47:54 +0300

    func: add test_invalid_log_in_user

--- tests/test_log_in_user.py
+++ tests/test_log_in_user.py
@@ -11,3 +11,6 @@ class TestLogin:
         response = requests.post(f'{Urls.BASE_URL}{Endpoints.LOGIN}', data=User.current_user)
         assert response.status_code == 200 and response.json().get('success') == True
 
+    def test_invalid_log_in_user(self):
+        response = requests.post(f'{Urls.BASE_URL}{Endpoints.LOGIN}', data=User.not_reg_user)
+        assert response.status_code == 401 and response.json().get('success') == False
