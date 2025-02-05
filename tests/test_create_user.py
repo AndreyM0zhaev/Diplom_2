b4769df6ad38f0375e0c4166c6a2c26c161ea1b4
Andrey Mozhaev <Mozhaev.andrey.v@yandex.com>
2025-02-05 23:26:19 +0300

    func: add test_double_user_registration

--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -9,3 +9,9 @@ class TestCreateUser:
     def test_new_user_registration(self):
         response = requests.post(f'{Urls.BASE_URL}{Endpoints.CREATE_USER}', data=User.registration_new_user())
         assert response.status_code == 200 and response.json()["success"] is True
+
+
+    def test_double_user_registration(self):
+        response = requests.post(f'{Urls.BASE_URL}{Endpoints.CREATE_USER}', data=User.double_user)
+        assert response.status_code == 403 and 'User already exists' in response.text
+
