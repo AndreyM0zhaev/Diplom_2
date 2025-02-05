a5f31a9cecbca6cd1a93a45f6902c94dd112c150
Andrey Mozhaev <Mozhaev.andrey.v@yandex.com>
2025-02-05 22:54:37 +0300

    func: update conftest

--- aps/endpoints.py
+++ aps/endpoints.py
@@ -1,5 +1,9 @@
-class Url:
+class Urls:
     BASE_URL = 'https://stellarburgers.nomoreparties.site/'
 
 class Endpoints:
+    headers = {"Content-Type": "application/json"}
     CREATE_USER = '/api/auth/register'
+    LOGIN = '/api/auth/login'
+    CHANGE_USER_DATA = '/api/auth/user'
+    DELETE_USER = '/api/auth/user'
